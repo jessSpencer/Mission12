6a30bccbd277117dea7469804bb175a6e386fe60
0captjack <85979485+0captjack@users.noreply.github.com>
2022-03-22 14:36:19 -0600

    made booking model

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mission12.Models
 {
-    public class Booking
+    public class Booking //information about the group
     {
+
+
+        [Key]
+        [Required]
+        public int BookID {get; set;}
+        [Key]
+        [Required]
+        public int AppointmentID { get; set; }
+
+        public string GroupName { get; set; }
+        [Required(ErrorMessage = "Please enter an Author")]
+        public int GroupSize { get; set; }
+
+        public string Email { get; set; }
+
+        public int Phone { get; set; }
+
+
     }
 }
+
